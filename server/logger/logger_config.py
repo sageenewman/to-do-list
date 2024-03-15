86014f4c66e4378f9deed16b7258e7c4063fd696
sagee newman <sagee2001@gmail.com>
2024-03-15 14:53:53 +0200

    logs path config

--- server/logger/logger_config.py
+++ server/logger/logger_config.py
@@ -1,3 +1,3 @@
 LOG_FORMAT = "{time:YYYY-MM-DD HH:mm:ss} - {level.icon} - {file}:{module}:{function}:{line}:{exception} - {message}"
-LOG_FILE_FORMAT = ".\\app_{time:YYYY-MM-DD}.log"
+LOG_FILE_FORMAT = ".logs\\app_{time:YYYY-MM-DD}.logs"
 MAX_SIZE_FILE = 1000000
