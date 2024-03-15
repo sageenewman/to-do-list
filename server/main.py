47c1d9dbd36d59455b6f4164c114294a36952474
sagee newman <sagee2001@gmail.com>
2024-03-15 14:39:00 +0200

    initialize_logger

--- server/main.py
+++ server/main.py
@@ -1,7 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+from logger.logger import initialize_logger
 
 app = FastAPI()
 
-if __name__ == '__main__':
+
+def main():
+    initialize_logger()
+    run_server()
+
+
+def run_server():
     uvicorn.run("main:app", port=8080, host="127.0.0.1")
+
+
+if __name__ == '__main__':
+    main()
