a55dd16895d79d99a65a786853e9b2d26389f3c8
sagee newman <sagee2001@gmail.com>
2024-03-15 15:05:20 +0200

    reformat file

--- server/logger/logger_config.py
+++ server/logger/logger_config.py
@@ -1,3 +1,3 @@
 LOG_FORMAT = "{time:YYYY-MM-DD HH:mm:ss} - {level.icon} - {file}:{module}:{function}:{line}:{exception} - {message}"
 LOG_FILE_FORMAT = ".\\logs\\app_{time:YYYY-MM-DD}.logs"
-MAX_SIZE_FILE = 1000000
+MAX_SIZE_FILE = 1_000_000
