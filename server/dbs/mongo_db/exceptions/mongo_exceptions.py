2c3b8596d06d66da8f0683ad66e3ec99ce56841e
sagee newman <sagee2001@gmail.com>
2024-03-16 20:46:36 +0200

    exceptions message

--- server/dbs/mongo_db/exceptions/mongo_exceptions.py
+++ server/dbs/mongo_db/exceptions/mongo_exceptions.py
@@ -1,10 +1,10 @@
 class DBNoneError(Exception):
-    def __init__(self, message="DB cannot be None"):
+    def __init__(self, message="DB cannot be None,pleas set db"):
         self.message = message
         super().__init__(self.message)
 
 
 class CollectionNoneError(Exception):
-    def __init__(self, message="collection cannot be None"):
+    def __init__(self, message="collection cannot be None,pleas set collection"):
         self.message = message
         super().__init__(self.message)
