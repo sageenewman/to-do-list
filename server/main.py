3c727d9770f502c7f2e80f76fbd96f5acf98294f
sagee newman <sagee2001@gmail.com>
2024-03-16 20:52:23 +0200

    include mission router

--- server/main.py
+++ server/main.py
@@ -4,9 +4,10 @@ from loguru import logger
 
 from app_config import APP, PORT, HOST
 from logger.logger import initialize_logger
+from mission.mission_router import mission_router
 
 app = FastAPI()
-
+app.include_router(mission_router)
 
 def main():
     initialize_logger()
